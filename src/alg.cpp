a6d37e3e8d83ce3df58b29ac772cacf03ec3352f
GolubDenis <99288665+GolubDenis@users.noreply.github.com>
2022-02-27 21:49:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,39 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value == 1) {
+        return false;
+    }
+    for (uint64_t i = 2; i <= value/2; i++)
+        if (value % i == 0)
+            return false;
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t i = 2;
+    uint64_t a = 1;
+    do {
+        nextPrime(i);
+        a + 1;
+    } while (a != n);
+    return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    uint64_t i = value;
+    do {
+        i += 1;
+    } while (!checkPrime(i));
+             return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t sum = 0;
+    uint64_t i = 0;
+    do {
+        sum += i;
+        i = nextPrime(i);
+    } while (i < hbound);
+    return sum;
 }
